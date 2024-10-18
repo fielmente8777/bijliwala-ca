c7b2cce61ee0434757081a22b05187e199482d49
sumit <sk7627577@gmail.com>
2024-10-18 13:23:22 +0530

    update inst profile

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -8,7 +8,20 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Grid, Navigation, Pagination } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 const Insta = () => {
+
+    const [posts, setPosts] = useState([]);
+    const accessToken = '';
+
+    useEffect(() => {
+        fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,permalink&access_token=${accessToken}`)
+            .then((response) => response.json())
+            .then((data) => setPosts(data.data))
+            .catch((error) => console.error(error));
+    }, [accessToken]);
+
+
     return (
         <section className="layout-section">
             <div className="max-width bg-no-repeat lg:bg-cover py-16" >
@@ -29,10 +42,14 @@ const Insta = () => {
                         modules={[Grid, Pagination, Navigation]}
                         className="insta"
                     >
-                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item) => (
-                            <SwiperSlide key={item}>
-                                <div className="relative w-full aspect-[3/3]">
-                                    <img src="https://picsum.photos/id/1018/200/300" alt="" className='object-cover absolute top-0 left-0 w-full h-full' />
+                        {posts.map((post) => (
+                            <SwiperSlide key={post.id} >
+                                <div to={post.permalink} target="_blank" rel="noopener noreferrer" className="relative w-full aspect-[3/3]">
+                                    {post.media_type === 'IMAGE' || post.media_type === 'CAROUSEL_ALBUM' ? (
+                                        <img src={post.media_url} alt="Instagram post" className='object-cover absolute top-0 left-0 w-full h-full' />
+                                    ) : post.media_type === 'VIDEO' ? (
+                                        <video src={post.media_url} controls className='object-cover absolute top-0 left-0 w-full h-full' />
+                                    ) : null}
                                 </div>
                             </SwiperSlide>
                         ))}
@@ -45,3 +62,6 @@ const Insta = () => {
 }
 
 export default Insta
+
+
+
